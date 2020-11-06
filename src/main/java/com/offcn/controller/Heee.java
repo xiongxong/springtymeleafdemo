cdc34f57164d618b7fa0d69215aca8fc8a23f40d
xiongxiong-java <1821496769@qq.com>
2020-11-06 17:24:35 +0800

    新增测试工具类

---
+++ src/main/java/com/offcn/controller/Heee.java
@@ -0,0 +1,10 @@
+package com.offcn.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Heee {
+    public  void hello(){
+        System.out.println("luelueleu");
+    }
+}
